70860ce0d51943bf4aa3a1d835c6d03a2afc36f8
Haydee Rodriguez <hrodriguez@Haydees-MacBook-Pro.local>
2018-03-21 09:54:25 -0600

    Project structure, in this point we see the structure of project and we ask us what we need for our game

---
+++ AimForThat-WS/AimForThat-WS/GameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AimForThat-WS
+//
+//  Created by Haydee Rodriguez on 3/20/18.
+//  Copyright © 2018 Haydee Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
